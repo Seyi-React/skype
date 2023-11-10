39d382f7c06f530d6abece1d28961cdf5cdb7890
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-11-10 02:56:30 +0100

    trensComponent-commenced

--- src/components/Trends/TrendsComponent.tsx
+++ src/components/Trends/TrendsComponent.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { MdNotifications } from 'react-icons/md'
+import styles from './TrendsComponent.module.css'
 
 const TrendsComponent = () => {
   return (
-    <div>TrendsComponent</div>
+    <div className={styles.trends}>
+         <div className="search-bar">
+             <div className="search-notif">
+                <input type="text" name="" id="" />
+             </div>
+             <div>
+                <MdNotifications />
+             </div>
+         </div>
+    </div>
   )
 }
 
