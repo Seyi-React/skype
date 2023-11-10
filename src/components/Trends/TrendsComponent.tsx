2e138c4789dee47799677a0f1bd51d38074ca018
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-11-10 13:48:24 +0100

    trends-components-on-the-run

--- src/components/Trends/TrendsComponent.tsx
+++ src/components/Trends/TrendsComponent.tsx
@@ -1,15 +1,16 @@
-import { MdNotifications } from 'react-icons/md'
+
 import styles from './TrendsComponent.module.css'
+import { IoIosNotificationsOutline } from 'react-icons/io'
 
 const TrendsComponent = () => {
   return (
     <div className={styles.trends}>
-         <div className="search-bar">
-             <div className="search-notif">
-                <input type="text" name="" id="" />
+         <div className={styles.search_bar}>
+             <div className={styles.search_notif}>
+                <input type="text" name="" id="" placeholder='Search trends' />
              </div>
              <div>
-                <MdNotifications />
+                <IoIosNotificationsOutline size={27} />
              </div>
          </div>
     </div>
