50638dfbab678a23b1e5f2935ae3cef9dbe2786e
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-11-13 00:32:04 +0100

    postcomponent-in-progress

--- src/components/Post/PostComponent.tsx
+++ src/components/Post/PostComponent.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import styles from './postComponent.module.css'
+import skype from '../../assets/Logo no background.webp'
 
 const PostComponent = () => {
   return (
-    <div>PostComponent</div>
+    <div className={styles.post}>
+      <div className={styles.posting}>
+        <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',gap:'3px'}}>
+          <img src={skype} alt="" width={60} />
+          <h5>All Post</h5>
+        </div>
+        <div style={{marginTop:'2rem',fontWeight:'bold',color:'gray',fontSize:'13px'}}>
+          PROJECT
+        </div>
+      </div>
+      <div>2</div>
+    </div>
   )
 }
 
