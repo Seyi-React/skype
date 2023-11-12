0fee193c543deb50acd9b20be12cedc61790bda5
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-11-13 00:37:24 +0100

    lists-added

--- src/components/Post/PostComponent.tsx
+++ src/components/Post/PostComponent.tsx
@@ -9,8 +9,28 @@ const PostComponent = () => {
           <img src={skype} alt="" width={60} />
           <h5>All Post</h5>
         </div>
-        <div style={{marginTop:'2rem',fontWeight:'bold',color:'gray',fontSize:'13px'}}>
+        <div style={{marginTop:'2rem',fontWeight:'bold',color:'gray',fontSize:'13px',padding:'1rem'}}>
           PROJECT
+        </div>
+        <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',gap:'3px'}}>
+          <img src={skype} alt="" width={60} />
+          <h5>Arena Sport</h5>
+        </div>
+        <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',gap:'3px'}}>
+          <img src={skype} alt="" width={60} />
+          <h5>DSV</h5>
+        </div>
+        <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',gap:'3px'}}>
+          <img src={skype} alt="" width={60} />
+          <h5>SeaFood Mall</h5>
+        </div>
+        <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',gap:'3px'}}>
+          <img src={skype} alt="" width={60} />
+          <h5>FireStar</h5>
+        </div>
+        <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',gap:'3px'}}>
+          <img src={skype} alt="" width={60} />
+          <h5>Zeta Bank</h5>
         </div>
       </div>
       <div>2</div>
